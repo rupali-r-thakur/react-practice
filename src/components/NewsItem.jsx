b8217aa40067980e4c89d7ecf09e2ff4b78cb709
rupali-r-thakur <rupali97097@gmail.com>
2024-03-20 12:02:44 +0530

    using fetch api to populate news items

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -3,12 +3,20 @@ import React from "react";
 function NewsItem(props) {
   return (
     <div class="card" style={{ width: "18rem" }}>
-      <img class="card-img-top" src={props.imageUrl} alt="Card image cap" />
+      <img
+        class="card-img-top"
+        src={
+          props.imageUrl
+            ? props.imageUrl
+            : "https://img.freepik.com/free-vector/realistic-news-studio-background_23-2149985612.jpg?size=626&ext=jpg&ga=GA1.1.735520172.1710892800&semt=ais"
+        }
+        alt="Card image cap"
+      />
       <div class="card-body">
-        <h5 class="card-title">{props.title}...</h5>
-        <p class="card-text">{props.description}...</p>
+        <h5 class="card-title">{props.title}</h5>
+        <p class="card-text">{props.description}</p>
         <a href={props.newsUrl} target="_blank" class="btn btn-primary">
-          Go somewhere
+          Read More
         </a>
       </div>
     </div>
